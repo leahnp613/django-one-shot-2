ee3b01ac82f402bf528da8ebc620c2a7ba0cbfd2
leahnp613 <leahnagarpowers@gmail.com>
2023-05-31 12:28:21 -0400

    Feature 6 Complete!

--- todos/admin.py
+++ todos/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from todos.models import TodoList
+from todos.models import TodoList, TodoItem
 
 
 # Register your models here.
 @admin.register(TodoList)
 class TodoListAdmin(admin.ModelAdmin):
     list_display = ("name",)
+
+@admin.register(TodoItem)
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ("task", "due_date", "is_completed",)
